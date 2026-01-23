ae3a23084caca856ba79d80413f23a1efcfbe9d9
pintujani <kpintu77673@gmail.com>
2026-01-23 21:27:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Fgru4laNvJpkMGI1")
 
 API_URL = getenv("API_URL", 'https://api2.nexgenbots.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.nexgenbots.xyz')
-API_KEY = getenv("API_KEY", "30DxNexGenBotsdc9b8a") # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+API_KEY = getenv("API_KEY", "30DxNexGenBotscedc05") # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
